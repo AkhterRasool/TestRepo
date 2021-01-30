20492708c55cf7b6f86d235a71a865648b81f92d
Mohammed Akhter Rasool <akhterrasool@Mohammeds-MacBook-Pro.local>
2021-01-30 17:04:11 +0530

    Develop test case to test sum of empty string.

---
+++ src/test/java/com/adecco/test/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+package com.adecco.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeAll
+    public static void setup() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void testSumOfEmptyString() {
+        String inputString = "";
+        int result = stringCalculator.add(inputString);
+        Assertions.assertEquals(0, result);
+    }
+
+}
