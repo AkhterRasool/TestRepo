2a5bec903133332a9437384c191afd0b617abcf3
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2021-01-30 17:21:15 +0530

    Write sufficient production code to make all tests pass.

--- src/main/java/com/adecco/test/StringCalculator.java
+++ src/main/java/com/adecco/test/StringCalculator.java
@@ -2,6 +2,14 @@ package com.adecco.test;
 
 public class StringCalculator {
     public int add(String numbers) {
+        if (numbers.contains(",")) {
+            String[] elems = numbers.trim().split(",");
+            int result = 0;
+            for (String elem: elems) {
+                result += Integer.parseInt(elem);
+            }
+            return result;
+        }
         return numbers.isEmpty() ? 0 : Integer.parseInt(numbers);
     }
 }
