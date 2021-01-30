a63e1e789e7206dba217a1452354babd063b2b01
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2021-01-30 18:25:19 +0530

    Write production code to make addNumbersWithFlexibleDelimiter() pass.

--- src/main/java/com/adecco/test/StringCalculator.java
+++ src/main/java/com/adecco/test/StringCalculator.java
@@ -2,7 +2,11 @@ package com.adecco.test;
 
 public class StringCalculator {
     public int add(String numbers) {
-        String[] elems = numbers.trim().split("[,\n]");
+        String delimiter = ",";
+        if (numbers.startsWith("//")) {
+            delimiter = String.valueOf(numbers.charAt(2));
+        }
+        String[] elems = numbers.trim().split("[" + delimiter + "\n]");
         int result = 0;
         for (String elem: elems) {
             try {
