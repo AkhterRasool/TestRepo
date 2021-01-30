33c3f8b496c0a11683855ef641f37a2b966ec8fd
Mohammed Akhter Rasool <akhterrasool48@gmail.com>
2021-01-30 18:13:59 +0530

    Write production code to make addNumbersHavingNewLines() pass.

--- src/main/java/com/adecco/test/StringCalculator.java
+++ src/main/java/com/adecco/test/StringCalculator.java
@@ -2,7 +2,7 @@ package com.adecco.test;
 
 public class StringCalculator {
     public int add(String numbers) {
-        String[] elems = numbers.trim().split(",");
+        String[] elems = numbers.trim().split("[,\n]");
         int result = 0;
         for (String elem: elems) {
             try {
